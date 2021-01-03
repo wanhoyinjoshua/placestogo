a3f837a6feb4d549ea34655780ab4289a0d78c99
wanhoyinjoshua <58977282+wanhoyinjoshua@users.noreply.github.com>
2021-01-04 08:25:55 +1100

    Add files via upload

---
+++ idea.js
@@ -0,0 +1,52 @@
+var coll = document.getElementsByClassName("collapsible1");
+var coll2 = document.getElementsByClassName("collapsible2");
+var coll3 = document.getElementsByClassName("collapsible3");
+var coll4 = document.getElementsByClassName("collapsible4");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight){
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  }
+
+  for (i = 0; i < coll.length; i++) {
+    coll2[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight){
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  }
+  for (i = 0; i < coll.length; i++) {
+    coll3[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight){
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  }
+
+  for (i = 0; i < coll.length; i++) {
+    coll4[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight){
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  }
